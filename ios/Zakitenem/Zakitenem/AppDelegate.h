d971d53c83044f266c41a669ab3c25e7eb3e4182
Tamara Suslova <tsuslova@noveogroup.com>
2014-07-29 17:18:42 +0700

    +zakitenem ios

---
+++ ios/Zakitenem/Zakitenem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Zakitenem
+//
+//  Created by Toto on 29.07.14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/Zakitenem/Zakitenem/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  Zakitenem
+//
+//  Created by Toto on 29.07.14.
+//
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end

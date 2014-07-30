a0bd00a03e76cdec86e006889fd871dd70cd9d31
Tamara Suslova <tsuslova@noveogroup.com>
2014-07-30 12:55:49 +0700

    setup push notifications ios

--- ios/Zakitenem/Zakitenem/AppDelegate.m
+++ ios/Zakitenem/Zakitenem/AppDelegate.m
@@ -8,39 +8,49 @@
 
 #import "AppDelegate.h"
 
-//TBD: test code
-#import "GTLServiceApi.h"
-#import "GTLQueryApi.h"
-#import "GTLApiUserMessageLoginInfo.h"
-#import "GTLApiUserMessageUser.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:
+        UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge |
+        UIRemoteNotificationTypeSound];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
 
-    //TBD: test code
-    GTLServiceApi *service = [[GTLServiceApi alloc] init];
-    service.retryEnabled = YES;
-    
-    GTLApiUserMessageLoginInfo *loginInfo = [[GTLApiUserMessageLoginInfo alloc] init];
-    loginInfo.login = @"TestUser11";
-    loginInfo.password = @"123";
-    loginInfo.deviceId = @"dffo8447fhi37";
-    
-    GTLQueryApi *query = [GTLQueryApi queryForAuthWithObject:loginInfo];
-    [service executeQuery:query completionHandler:^(GTLServiceTicket *ticket,
-                                                    GTLApiUserMessageUser *obj, NSError *error){
-        DLOG(@"%@", [obj login]);
-        DLOG(@"%@", [error localizedDescription]);
-    }];
-    //TBD: test code
-    
     return YES;
 }
+
+#pragma mark - Push notifications
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    const unsigned char *dataBuffer = (const unsigned char *)[deviceToken bytes];
+    if (!dataBuffer) return;
+    
+    NSUInteger dataLength = [deviceToken length];
+    NSMutableString *tokenStr  = [NSMutableString stringWithCapacity:(dataLength * 2)];
+    for (int i = 0; i < dataLength; ++i) {
+        [tokenStr appendString:[NSString stringWithFormat:@"%02lx", (unsigned long)dataBuffer[i]]];
+    }
+    
+    DLOG(@"Push notifications token: %@", tokenStr);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    DLOG(@"Push notifications registration failed with error:%@", error);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    DLOG(@"Push userInfo %@ ", userInfo);
+}
+
 
 @end
